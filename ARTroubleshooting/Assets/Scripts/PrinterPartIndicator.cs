b8dc53a86515bb90f54a1a3a648d409a1da4d782
Noah Schoeters <noah.schoeters@student.uhasselt.be>
2025-05-02 16:47:51 +0200

    StartScript

---
+++ ARTroubleshooting/Assets/Scripts/PrinterPartIndicator.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrinterPartIndicator : MonoBehaviour
+{
+    public Material HighlightMaterial;
+
+    public List<GameObject> PrinterFrame;
+    public List<GameObject> Screen;
+    public List<GameObject> HeatBed;
+    public List<GameObject> Xaxis;
+    public List<GameObject> Toolhead;
+    public List<GameObject> ElectricalCord;
+    public List<GameObject> FillamentCutter;
+    public List<GameObject> PurgeWiper;
+    public List<GameObject> LiveVieuwCamera;
+    public List<GameObject> Nozzle;
+    public List<GameObject> SiliconeSockforHotend;
+    public List<GameObject> PartCoolingFan;
+    public List<GameObject> USBCcable;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
