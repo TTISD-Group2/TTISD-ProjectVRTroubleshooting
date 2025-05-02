c4fae4f7e8849d3f4d990ca384bf8984eb8450e5
Noah Schoeters <noah.schoeters@student.uhasselt.be>
2025-05-02 21:55:34 +0200

    updated script

--- ARTroubleshooting/Assets/Scripts/PrinterPartIndicator.cs
+++ ARTroubleshooting/Assets/Scripts/PrinterPartIndicator.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class PrinterPartIndicator : MonoBehaviour
 {
     public Material HighlightMaterial;
+    public Material NonHighLight;
 
     public List<GameObject> PrinterFrame;
     public List<GameObject> Screen;
@@ -23,7 +24,7 @@ public class PrinterPartIndicator : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        hightLightFrame();
     }
 
     // Update is called once per frame
@@ -31,4 +32,140 @@ public class PrinterPartIndicator : MonoBehaviour
     {
         
     }
+
+
+    private void makeAllHighLight(List<GameObject> objects){
+
+        foreach (GameObject objecty in objects){
+            //objecty.material = HighlightMaterial;
+             Renderer renderer = objecty.GetComponent<Renderer>();
+            if (renderer != null)
+            {
+                renderer.material = HighlightMaterial;
+            }
+        }
+
+    }
+
+    private void makeAllNotHighLight(List<GameObject> objects){
+
+        foreach (GameObject objecty in objects){
+             Renderer renderer = objecty.GetComponent<Renderer>();
+            if (renderer != null)
+            {
+                renderer.material = NonHighLight;
+            }
+        }
+
+    }
+
+    public void hightLightFrame(){
+        makeAllHighLight(PrinterFrame);
+    }
+
+    public void unHightLightFrame(){
+        makeAllNotHighLight(PrinterFrame);
+    }
+
+    public void hightLightScreen(){
+        makeAllHighLight(Screen);
+    }
+
+    public void unHightLightScreen(){
+        makeAllNotHighLight(Screen);
+    }
+
+
+    public void hightLightHeatBed(){
+        makeAllHighLight(HeatBed);
+    }
+
+    public void unHightHeatBed(){
+        makeAllNotHighLight(HeatBed);
+    }
+
+    public void hightLightXaxis(){
+        makeAllHighLight(Xaxis);
+    }
+
+    public void unHightXaxis(){
+        makeAllNotHighLight(Xaxis);
+    }
+
+    public void hightLightToolhead(){
+        makeAllHighLight(Toolhead);
+    }
+
+    public void unHightToolhead(){
+        makeAllNotHighLight(Toolhead);
+    }
+
+    public void hightLightElectricalCord(){
+        makeAllHighLight(ElectricalCord);
+    }
+
+    public void unHightElectricalCord(){
+        makeAllNotHighLight(ElectricalCord);
+    }
+
+    public void hightLightFillamentCutter(){
+        makeAllHighLight(FillamentCutter);
+    }
+
+    public void unHightFillamentCutter(){
+        makeAllNotHighLight(FillamentCutter);
+    }
+
+
+    public void hightLightPurgeWiper(){
+        makeAllHighLight(PurgeWiper);
+    }
+
+    public void unHightPurgeWiper(){
+        makeAllNotHighLight(PurgeWiper);
+    }
+
+
+    public void hightLightLiveVieuwCamera(){
+        makeAllHighLight(LiveVieuwCamera);
+    }
+
+    public void unHightLiveVieuwCamera(){
+        makeAllNotHighLight(LiveVieuwCamera);
+    }
+
+
+    public void hightLightNozzle(){
+        makeAllHighLight(Nozzle);
+    }
+
+    public void unHightNozzle(){
+        makeAllNotHighLight(Nozzle);
+    }
+
+    public void hightSiliconeSockforHotend(){
+        makeAllHighLight(SiliconeSockforHotend);
+    }
+
+    public void unHightSiliconeSockforHotend(){
+        makeAllNotHighLight(SiliconeSockforHotend);
+    }
+
+    public void hightSiliconePartCoolingFan(){
+        makeAllHighLight(PartCoolingFan);
+    }
+
+    public void unHightPartCoolingFan(){
+        makeAllNotHighLight(PartCoolingFan);
+    }
+
+    public void hightUSBCcable(){
+        makeAllHighLight(USBCcable);
+    }
+
+    public void unHightUSBCcable(){
+        makeAllNotHighLight(USBCcable);
+    }
+
+
 }
